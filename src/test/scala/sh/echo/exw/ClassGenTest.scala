09872d65709a03c03267c9c43cb98686861d245c
Jonathan Chow <me@echo.sh>
2014-08-03 15:08:18 +1200

    Splits generic codegen test into base trait.

--- src/test/scala/sh/echo/exw/ClassGenTest.scala
+++ src/test/scala/sh/echo/exw/ClassGenTest.scala
@@ -1,31 +1,6 @@
 package sh.echo.exw
 
-import scala.collection.JavaConversions._
-import scala.collection.mutable
-
-import org.objectweb.asm._
-import org.objectweb.asm.util._
-import org.scalatest._
-
-object ClassGenTest {
-  import ClassGen.paramsFor
-  def checkCast(tpe: String) = {
-    val (ot, _) = paramsFor(tpe)
-    s"CHECKCAST L$ot;"
-  }
-  def unbox(tpe: String) = {
-    val (ot, em) = paramsFor(tpe)
-    s"INVOKEVIRTUAL $ot.$em ()$tpe"
-  }
-  def box(tpe: String) = {
-    val (ot, _) = paramsFor(tpe)
-    s"INVOKESTATIC $ot.valueOf ($tpe)L$ot;"
-  }
-}
-
-class ClassGenTest extends FunSpec with ShouldMatchers {
-  import ClassGen._
-  import ClassGenTest._
+class AddGenTest extends GenTestBase {
 
   it("compiles a simple addition function") {
     val ms = compile {
@@ -119,42 +94,5 @@ class ClassGenTest extends FunSpec with ShouldMatchers {
       box("J"),
       "ARETURN"
     )
-  }
-
-  def compile(lisp: String): Map[String, List[String]] = {
-    val lp = new LispParser
-    val cg = new ClassGen
-    val bc = cg.compile(lp.parse(lisp))
-    InsnExtractor.parse(bc)
-  }
-}
-
-object InsnExtractor {
-  def parse(bc: Array[Byte]): Map[String, List[String]] = {
-    val cr = new ClassReader(bc)
-    val ex = new InsnExtractor()
-    cr.accept(ex, 0)
-    ex.methods
-  }
-}
-class InsnExtractor extends ClassVisitor(Opcodes.ASM5) {
-  private val data = mutable.Map.empty[String, Textifier]
-
-  def methods: Map[String, List[String]] =
-    data.mapValues { t ⇒
-      val data = t.getText.map(_.toString.trim).toList
-      data.filterNot(i ⇒ Set("MAXSTACK", "MAXLOCALS").exists(i.contains))
-    }.toMap
-
-  override def visitMethod(
-    access: Int,
-    name: String,
-    desc: String,
-    signature: String,
-    exceptions: Array[String]): MethodVisitor = {
-
-    val key = name + desc
-    val t = data.getOrElseUpdate(key, new Textifier())
-    new TraceMethodVisitor(t)
   }
 }
