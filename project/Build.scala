26278b711eeaa8a570dc2aa2b83e718c4ea5f67a
Jonathan Chow <me@echo.sh>
2014-08-03 18:26:20 +1200

    Changes deps the other way around.

--- project/Build.scala
+++ project/Build.scala
@@ -2,11 +2,11 @@ import sbt._
 import Keys._
 
 object HelloBuild extends Build {
-  lazy val stdlib = (
-    project in file("stdlib")
-  )
   lazy val compiler = (
     project in file("compiler")
-      dependsOn stdlib
+  )
+  lazy val stdlib = (
+    project in file("stdlib")
+      dependsOn compiler
   )
 }
