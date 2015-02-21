8bf8e751a2547c866eb4ca9f95937f4a3adea223
Jonathan Chow <me@echo.sh>
2015-02-22 10:02:31 +1300

    Some syntax support.

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,11 +2,18 @@ import scala.util.parsing.combinator._
 
 object Parser extends JavaTokenParsers {
 
-  def parse(code: String): Any = super.parse(expr, code).get
+  def parse(code: String): ParseResult[Any] = super.parse(expr, code)
 
-  def expr = number | string | symbol
+  def expr = number | string | symbol | list | quotes
 
-  def number: Parser[Any] = """-?(\d+(\.\d*)?|\d*\.\d+)""".r ^^ (_.toDouble)
+  def number: Parser[Any] = """-?(\d+(\.\d*)?|\d*\.\d+)\b""".r ^^ (_.toDouble)
   def string: Parser[Any] = stringLiteral ^^ (s ⇒ s.substring(1, s.length - 1))
-  def symbol: Parser[Any] = ident ^^ Symbol.apply
+  def symbol: Parser[Any] = """[^\d\(\)'`,\s][^\(\)'`,\s]*""".r ^^ Symbol.apply
+  def list: Parser[Any] = "(" ~> rep(expr) <~ ")"
+
+  def quotes: Parser[Any] = quote | quasiquote | unquote
+
+  def quote: Parser[Any] = "'" ~> expr ^^ (e ⇒ List('quote, e))
+  def quasiquote: Parser[Any] = "`" ~> expr ^^ (e ⇒ List('quasiquote, e))
+  def unquote: Parser[Any] = "," ~> expr ^^ (e ⇒ List('unquote, e))
 }
--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,7 +1,8 @@
 import org.scalatest._
 
 class ParserTest extends FunSpec with ShouldMatchers {
-  import Parser.parse
+
+  def parse(code: String): Any = Parser.parse(code).get
 
   describe("numbers") {
     it("parses a decimal as double") {
@@ -16,6 +17,9 @@ class ParserTest extends FunSpec with ShouldMatchers {
     it("parses a negative integer as double") {
       parse("-1") shouldBe -1.0
     }
+    it("errors if a number isn't followed by a word boundary") {
+      Parser.parse("3a") shouldBe a[Parser.NoSuccess]
+    }
   }
 
   describe("strings") {
@@ -25,11 +29,66 @@ class ParserTest extends FunSpec with ShouldMatchers {
     it("parses an empty string") {
       parse("\"\"") shouldBe ""
     }
+    it("parses escaped characters")(pending)
   }
 
   describe("symbols") {
     it("parses a symbol") {
       parse("world") shouldEqual 'world
+    }
+    it("parses special characters as symbols") {
+      parse("+") shouldEqual Symbol("+")
+    }
+    it("parses special characters as part of symbols") {
+      parse("nil?") shouldEqual Symbol("nil?")
+    }
+  }
+
+  describe("lists") {
+    it("parses an empty list") {
+      parse("()") shouldBe Nil
+    }
+    it("parses a list with one expr") {
+      parse("(foo)") shouldBe List('foo)
+    }
+    it("parses a list with more than one expr") {
+      parse("(foo 1 2.5 \"3\")") shouldBe List('foo, 1.0, 2.5, "3")
+    }
+    it("parses nested lists") {
+      parse("(foo () (1 2.5 \"3\") bar)") shouldBe List('foo, Nil, List(1.0, 2.5, "3"), 'bar)
+    }
+  }
+
+  describe("quotes") {
+    Seq(
+      "'" → "quote",
+      "`" → "quasiquote",
+      "," → "unquote"
+    ) foreach { case (char, name) ⇒
+      it(s"parses $char on a single expr into $name") {
+        parse(s"${char}foo") shouldBe List(Symbol(name), 'foo)
+      }
+      it(s"parses $char on a list into $name") {
+        parse(s"$char(foo bar)") shouldBe List(Symbol(name), List('foo, 'bar))
+      }
+      it(s"parses nested $char") {
+        parse(s"${char}${char}foo") shouldBe List(Symbol(name), List(Symbol(name), 'foo))
+      }
+    }
+  }
+
+  describe("integration") {
+    it("parses a typical expression") {
+      parse { """
+        (def (foldl fun zero xs)
+          (if (nil? xs)
+            zero
+            (fun (foldl fun zero (cdr xs)) (car xs))))
+      """ } shouldBe
+      List('def, List('foldl, 'fun, 'zero, 'xs),
+        List('if, List(Symbol("nil?"), 'xs),
+          'zero,
+          List('fun, List('foldl, 'fun, 'zero, List('cdr, 'xs)), List('car, 'xs))))
     }
   }
 }
